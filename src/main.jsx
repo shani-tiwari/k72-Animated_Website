370ae3d612359b0548d81129afacecb8213313e6
shani-tiwari <shanitiwari2021@gmail.com>
2025-09-19 17:55:26 +0530

    fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Stair from './components/common/Stair.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import NavContext from './context/NavContext.jsx'
+import Stair from './components//common/Stair.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
